8833c1d0651ffa06121fad4e8de1e48400d69ac0
The-DarK-os <anisri477@gmail.com>
2023-07-26 12:32:03 +0000

    Rewrite fitness and finding data influential

--- sources/Fitness.py
+++ sources/Fitness.py
@@ -1,7 +1,9 @@
 def FitnessFunc(In_degree,Out_degree,Graph):
     Nodes_ = []
     Fitness_ = []
-    for node in Graph.nodes():
+    Edges_ = []
+    for node in list(Graph.nodes()):
+        Edges_.append(Graph.edges(node))
         Nodes_.append(node)
         Fitness_.append(In_degree[node]/(In_degree[node] + Out_degree[node])**2)
-    return Nodes_,Fitness_
+    return Nodes_,Fitness_,Edges_
