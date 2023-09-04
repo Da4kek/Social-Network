5f0367e963ecca12e8af324119d6904a07cc9f08
The-DarK-os <anisri477@gmail.com>
2023-09-04 15:58:38 +0000

    Complete refurbish of code

--- sources/Fitness.py
+++ sources/Fitness.py
@@ -1,9 +1,4 @@
-def FitnessFunc(In_degree,Out_degree,Graph):
-    Nodes_ = []
-    Fitness_ = []
-    Edges_ = []
-    for node in list(Graph.nodes()):
-        Edges_.append(Graph.edges(node))
-        Nodes_.append(node)
-        Fitness_.append(In_degree[node]/(In_degree[node] + Out_degree[node])**2)
-    return Nodes_,Fitness_,Edges_
+def calculate_fitness(wi, fin, fout):
+    if fin == 0 and fout == 0:
+        return 0.0  
+    return wi * fin / (fin + fout) ** 2
