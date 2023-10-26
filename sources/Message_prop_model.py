8e83014292d5295b90f7279a00fcf28a22f84cea
Dark <anisri477@gmail.com>
2023-10-26 19:24:10 +0530

    Added truthfulness

--- sources/Message_prop_model.py
+++ sources/Message_prop_model.py
@@ -13,3 +13,24 @@ def message_prop(graph,seed_nodes,prob):
                 new_active_nodes.add(neighbor)
                 active_nodes.add(neighbor)
     return active_nodes
+
+def message_prop_T(graph,seed_nodes,threshold):
+    activated_nodes = seed_nodes
+    new_activated_nodes = seed_nodes
+
+    while new_activated_nodes:
+        new_activated_nodes = []
+
+        for node in activated_nodes:
+            neighbors = list(graph.neighbors(node))
+
+            for neighbor in neighbors:
+                if (
+                    graph.nodes[node].get('fitness',0) >= 0.2
+                    and neighbor not in activated_nodes
+                    and neighbor not in new_activated_nodes
+                ):
+                    new_activated_nodes.append(neighbor)
+
+        activated_nodes.extend(new_activated_nodes)
+    return activated_nodes
